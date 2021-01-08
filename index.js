9d113a9be8dc505bd05a522228a655049c9c5683
A7GoD <60264425+A7GoD@users.noreply.github.com>
2021-01-08 20:21:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require("./config.json");
 const custom = require("./custom.json");
 const discord = require("discord.js");
 const tts = require("discord-tts");
@@ -5,7 +6,9 @@ const tts = require("discord-tts");
 const client = new discord.Client();
 
 let prefix = "~";
-let delay = 500;
+let delay = 1000;
+
+let stream = null;
 
 const messages = custom.joinMessages;
 
@@ -19,45 +22,58 @@ client.on("message", (msg) => {
   }
 });
 
-client.on("voiceStateUpdate", (prevState, newState) => {
+client.on("voiceStateUpdate", async (prevState, newState) => {
   const broadcast = client.voice.createBroadcast();
-
   if (
     channelCheck(prevState, newState) === "joined" &&
     (newState.channelID ?? 0) &&
     newState.member.id !== "776437574948356107"
   ) {
-    newState.channel.join().then((con) => {
-      const ob = messages.find((user) => user.id === newState.member.id);
-      if (ob) {
-        setTimeout(() => broadcast.play(tts.getVoiceStream(ob.msg)), delay);
-      } else {
-        setTimeout(
-          () =>
-            broadcast.play(
-              tts.getVoiceStream(
-                `${
-                  newState.member.nickname ?? newState.member.displayName
-                } has joined.`
-              )
-            ),
-          delay
-        );
-      }
-      const dispatcher = con.play(broadcast);
-    });
+    newState.channel
+      .join()
+      .then((con) => {
+        con.setSpeaking("SPEAKING");
+        if (stream ?? 0) stream.end();
+        const ob = messages.find((user) => user.id === newState.member.id);
+        if (ob) {
+          broadcast.end();
+          setTimeout(() => {
+            broadcast.play(tts.getVoiceStream(ob.msg));
+          }, delay);
+        } else {
+          setTimeout(
+            () =>
+              broadcast.play(
+                tts.getVoiceStream(
+                  `${
+                    newState.member.nickname ?? newState.member.displayName
+                  } has joined.`
+                )
+              ),
+            delay
+          );
+        }
+        const dispatcher = con.play(broadcast);
+        stream = dispatcher;
+      })
+      .catch(() => console.log("error"));
   } else if (
     channelCheck(prevState, newState) === "left" &&
     newState.member.id !== "776437574948356107"
   ) {
-    prevState.channel.join().then((con) => {
-      broadcast.play(
-        tts.getVoiceStream(
-          `${newState.member.nickname ?? newState.member.displayName} has left.`
-        )
-      );
-      const dispatcher = con.play(broadcast);
-    });
+    prevState.channel
+      .join()
+      .then((con) => {
+        broadcast.play(
+          tts.getVoiceStream(
+            `${
+              newState.member.nickname ?? newState.member.displayName
+            } has left.`
+          )
+        );
+        const dispatcher = con.play(broadcast);
+      })
+      .catch(() => console.log("error"));
   }
 });
 
