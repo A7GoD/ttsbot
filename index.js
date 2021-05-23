a70f655f9f5c51266f5114ae499c228097f9582d
A7GoD <60264425+A7GoD@users.noreply.github.com>
2021-05-23 11:32:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ client.on("message", (msg) => {
     switch (command) {
       case "say":
         sentence = message.replace("~say ", "");
+        console.log(setence);
         const broadcast = client.voice.createBroadcast();
         const user = msg.guild.member(msg.author);
         if (user.voice.channel) {
