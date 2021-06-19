1464f0ec25155cfd1e75c6698f4ccfcd57be4bb0
A7GoD <60264425+A7GoD@users.noreply.github.com>
2021-06-19 17:04:14 +0530

    ...

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ client.on("message", (msg) => {
 							) {
 								console.log(`saying: ${sentence}`);
 								broadcast.play(
-									tts.getVoiceStream(`${sentence}`, TTSOptions)
+									tts.getVoiceStream(sentence, TTSOptions)
 								)
 							} else {
 								broadcast.play(
